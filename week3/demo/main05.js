329f6de544095ffc6876b36126cb4a76ae2bbe12
ISHEET SHETTY <64032564+isheet1910@users.noreply.github.com>
2022-02-16 17:55:42 +0530

    ..

---
+++ week3/demo/main05.js
@@ -0,0 +1,11 @@
+demo05
+//main.js 
+//anonymous function
+//Anonymous function : function without name
+// we have created  a variable referencing to a function
+// in below code app is a function variable
+const app = function() {
+    console.log("Function without name is called");
+}
+
+app();
---
+++ week3/demo/main06.js
@@ -0,0 +1,15 @@
+demo06
+//main.js
+//callback function
+function f1() {
+    console.log("We will call this function f1() in other functions");
+}
+
+function doSomething(functionName) {
+    console.log("Doing something");
+    console.log("calling function whoes named is passed in functioname");
+    functionName();
+    console.log("Work completed!");
+}
+
+doSomething(f1);
