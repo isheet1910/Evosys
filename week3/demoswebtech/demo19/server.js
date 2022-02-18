5712564d36e3e64aee62a9c33d4063158a16d500
ISHEET SHETTY <64032564+isheet1910@users.noreply.github.com>
2022-02-18 17:56:29 +0530

    .

--- week3/demoswebtech/demo19/server.js
+++ week3/demoswebtech/demo19/server.js
@@ -1,15 +1,15 @@
-const http=require('http');
-const fs=require('fs');
-const port=3000;
-const serverMessage=`http://localhost:${port} started`;
-function myfunction(file_name,content_type,resp){
-    fs.readFile(file_name,(err,data)=>{
-        resp.setHeader('Content-Type',content_type);
-        if(err){
-            resp.write(err);                     
-        }
-        else{                    
-            resp.write(data);                                        
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+const serverMessage = `http://localhost:${port} started`;
+
+function myfunction(file_name, content_type, resp) {
+    fs.readFile(file_name, (err, data) => {
+        resp.setHeader('Content-Type', content_type);
+        if (err) {
+            resp.write(err);
+        } else {
+            resp.write(data);
         }
         resp.end();
 
@@ -17,41 +17,41 @@ function myfunction(file_name,content_type,resp){
 }
 
 
-http.createServer((req,resp)=>{
-    const url=req.url;
-    const method=req.method;
+http.createServer((req, resp) => {
+    const url = req.url;
+    const method = req.method;
     // //Below code explains what is url and method in request
     // resp.setHeader('Content-Type','text/html')
     // resp.write("<br/>req.url: "+url);
     // resp.write("<br/>req.method: "+method);
     // resp.end();
 
-    switch(url){
+    switch (url) {
         case '/':
         case '/index.html':
-            myfunction(__dirname+'/index.html','text/html',resp);
+            myfunction(__dirname + '/index.html', 'text/html', resp);
             break;
         case '/aboutus.html':
-            myfunction(__dirname+'/aboutus.html','text/html',resp);
-            break;            
+            myfunction(__dirname + '/aboutus.html', 'text/html', resp);
+            break;
         case '/contactus.html':
-            
-        myfunction(__dirname+'/contactus.html','text/html',resp);
+
+            myfunction(__dirname + '/contactus.html', 'text/html', resp);
             break;
         case '/signup.html':
-            myfunction(__dirname+'/signup.html','text/html',resp);
+            myfunction(__dirname + '/signup.html', 'text/html', resp);
             break;
-        case '/signin.html':    
-        myfunction(__dirname+'/signin.html','text/html',resp);
+        case '/signin.html':
+            myfunction(__dirname + '/signin.html', 'text/html', resp);
             break;
         case '/style.css':
-            myfunction(__dirname+'/css/style.css','text/css',resp);
+            myfunction(__dirname + '/css/style.css', 'text/css', resp);
             break;
         default:
 
-                resp.write('Cotentn not found');
-                resp.end();
+            resp.write('Cotentn not found');
+            resp.end();
             break;
     }
 
-}).listen(port,()=>{console.log(serverMessage);});
+}).listen(port, () => { console.log(serverMessage); });
