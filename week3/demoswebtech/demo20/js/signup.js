f7c781df96b4cb7be1140fc54b95ec217e25b490
ISHEET SHETTY <64032564+isheet1910@users.noreply.github.com>
2022-02-22 15:59:33 +0530

    ..

--- week3/demoswebtech/demo20/js/signup.js
+++ week3/demoswebtech/demo20/js/signup.js
@@ -1,17 +1,17 @@
-function validation(event){
-    
+function validation(event) {
+
     alert("validation() Called!");
-    const password=document.getElementById("password").value;
-    const cpassword=document.getElementById("cpassword").value;
-    console.log("password: "+password+" cpassword"+cpassword);
-    const err_cpassword=document.getElementById("err_cpassword");
-    if(password===cpassword){
+    const password = document.getElementById("password").value;
+    const cpassword = document.getElementById("cpassword").value;
+    alert("password: " + password + " cpassword :" + cpassword);
+    const err_cpassword = document.getElementById("err_cpassword");
+    if (password === cpassword) {
         return true;
     }
     event.preventDefault();
-    err_cpassword.innerText="Password value does not match!";
-    err_cpassword.style.color="red";
-    
-    
+    err_cpassword.innerText = "Password value does not match!";
+    err_cpassword.style.color = "red";
+
+
     return false;
 }
