d5d22baa873f2df5e831ef68f20c2b7a6ee22a83
ISHEET SHETTY <64032564+isheet1910@users.noreply.github.com>
2022-02-28 18:15:22 +0530

    demo26

---
+++ week5/demoswebtech/demo26/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import router from './app/product_route.js';
+const port=4200;
+const message="http://localhost:4200/ website is hosted";
+const __dirname=path.resolve(path.dirname(''));
+const server=express();
+server.use(express.urlencoded({extended:true}));
+server.use(express.json());
+server.use('/app',router);
+server.get('/',(req,resp)=>{
+    resp.setHeader('Content-Type','text/html');
+    resp.sendFile(__dirname+'/index.html');
+});
+server.get('/style.css',(req,resp)=>{
+    resp.setHeader('Content-Type','text/css');
+    resp.sendFile(__dirname+'/css/style.css');
+});
+server.get('/main.js',(req,resp)=>{
+    resp.setHeader('Content-Type','application/javascript');
+    resp.sendFile(__dirname+'/js/main.js');
+});
+
+server.listen(port,()=>{console.log(message);})
