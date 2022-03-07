dff080147fff12c3603026e3812d63c82ceabf78
ISHEET SHETTY <64032564+isheet1910@users.noreply.github.com>
2022-03-07 18:22:12 +0530

    week 5-6 combine

--- week5/demo00/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-
-	public static void main(String [] args){
-		System.out.print  ("1- Hello World");
-		System.out.println("2- Hello World");
-		System.out.println("3- Hello World");
-	}
-}
--- week5/demo01/Main.java
+++
@@ -1,8 +0,0 @@
-package com.mastek.demo01;
-//com.mastek.demo01.Main
-public class Main{
-
-	public static void main(String [] args){
-		System.out.println("Hello World");
-	}
-}
